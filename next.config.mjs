6851ea5af924c9bb1a72f6f18e3fc57c09eb2bc5
Abdul Zakir <abdulzakir632@gmail.com>
2025-01-16 00:43:44 -0800

    next.config.mjs updated

--- next.config.mjs
+++ next.config.mjs
@@ -5,7 +5,7 @@ export default (phase, { defaultConfig }) => {
    * @type {import('next').NextConfig}
    */
   const nextConfig = {
-    // output: 'export', // Enable static export
+    output: 'export', // Enable static export
     /* config options here */
   };
   return nextConfig;
